eaaf41348b3888c1dbb43503a21ff86527e5be98
Vinaysohaliya <sohaliyavin01@gmail.com>
2024-11-24 18:47:43 +0530

    Auth added

---
+++ app/(routes)/dashboard/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Header from "@/app/_components/Header"
+
+function Dashboard() {
+
+  return (
+    <div className='p-8'>
+      <Header />
+      hi
+
+    </div>
+
+  )
+}
+
+export default Dashboard
